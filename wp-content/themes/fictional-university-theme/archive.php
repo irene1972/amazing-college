4d14acb2fe0ff6bdcff9f8898f11557e6232069b
Irene <ireneog_72@hotmail.es>
2020-01-14 11:39:09 +0100

    Add archive.php to overwrite index.php

---
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('/images/ocean.jpg')?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title"><?= the_archive_title() ?></h1><!--  (is_category()) ? single_cat_title() : ((is_author()) ? ('Posts by ' . the_author()) : '')  -->
+    <div class="page-banner__intro">
+      <p><?= the_archive_description() ?></p>
+    </div>
+  </div>  
+</div>
+
+<div class="container container--narrow page-section">
+  <?php 
+    while( have_posts() ){
+      the_post(); 
+  ?>
+  <div class="post-item">
+
+    <h2><a class="headline headline--medium headline--post-title" href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+    
+    <div class="metabox">
+      <p>Posted by <?php the_author_posts_link() ?> on <?php the_time('n/j/Y') ?> in <?= get_the_category_list(', ') ?></p>
+    </div>
+    
+    <div class="generic-content">
+      <!-- < ?php the_content(); ?> Nota: la función the_excerpt() muetra solo un trozo del content -->
+      <?php the_excerpt(); ?>
+      <p><a class="btn btn--blue" href="<?php the_permalink() ?>">Continue reading &raquo;</a></p>
+    </div>
+  </div>
+  <?php
+    }
+
+    echo paginate_links();
+  ?>
+</div>
+
+<?php get_footer(); ?>
