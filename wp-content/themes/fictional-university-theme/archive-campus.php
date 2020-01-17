f77f6086d7ef08159dd91333fb376d50ad7830a0
Irene <ireneog_72@hotmail.es>
2020-01-17 23:01:02 +0100

    Customize campus  archive

---
+++ wp-content/themes/fictional-university-theme/archive-campus.php
@@ -0,0 +1,43 @@
+<?php
+
+  get_header(); 
+  
+  pageBanner(array(
+    'title' => 'Our Campuses',
+    'subtitle' => 'We have several conveniently located campuses.'
+  ));
+
+?>
+
+<div class="container container--narrow page-section">
+  <div class="acf-map">
+  <!-- <ul class="link-list min-list"> -->
+    <?php 
+
+      while( have_posts() ){
+        
+        the_post();
+
+        $map_location = get_field('map_location');
+
+      // Lo hardcodeamos porque no tenemos la api-key de google
+      $map_location['lat'] = '40.78214997551874';
+      $map_location['lng'] = '-74.01127620000000';
+
+
+    ?>
+    <!-- <li><a href="< ?= the_permalink() ?>">< ?= the_title() ?></a></li> -->
+    <div class="marker" data-lat="<?= $map_location['lat'] ?>" data-lng="<?= $map_location['lng'] ?>"></div>
+    <?php
+
+      }?>
+  <!-- </ul> -->
+  </div>
+  <?php
+
+    echo paginate_links();
+  ?>
+
+</div>
+
+<?php get_footer(); ?>
