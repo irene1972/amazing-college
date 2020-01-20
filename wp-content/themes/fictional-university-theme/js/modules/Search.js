ca087ca153732056acd57e1e06fd9656a73173b4
Irene <ireneog_72@hotmail.es>
2020-01-20 12:01:16 +0100

    Open and Close search overly

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -1,7 +1,32 @@
+import $ from 'jquery';
+
 class Search{
 
+  //1. Describe ande create/iniciate our object
   constructor(){
-    alert('hello i am a search');
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.events();
+  }
+
+  // Events
+  events(){
+    this.openButton.on( 'click', this.openOverlay.bind(this) ); //---> el 'bind(this)' es para que cuando estemos dentro de la función 'openOverlay' al hacer mención a this entienda el this referido a la clase en la que estamos y no el this referido al elemento al que hemos hecho click!!
+    this.closeButton.on( 'click', this.closeOverlay.bind(this) );
+
+  }
+
+  //Methods (function, action...)
+  openOverlay(){
+    //console.log(this.searchOverlay)
+    this.searchOverlay.addClass('search-overlay--active');
+
+  }
+
+  closeOverlay(){
+    this.searchOverlay.removeClass('search-overlay--active');
+
   }
 
 }
