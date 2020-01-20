5a01e332cdf8257693acf3fc61dae5c3cd57ab15
Irene <ireneog_72@hotmail.es>
2020-01-20 22:18:47 +0100

    Get wp-json responses search from synchronous meth

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -62,17 +62,22 @@ class Search{
     // $.getJSON( 'http://localhost:3000/amazing-college/app/wp-json/wp/v2/posts?search=' + this.searchField.val(), function( posts ){
     //   alert(posts[0].title.rendered);
     // }.bind(this));
-  
+
     $.getJSON( universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
-      this.resultsDiv.html(`
-      <h2 class="search-overlay__section-title">General Information</h2>
-      ${( posts.length ) ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
-        ${posts.map(item=>`<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-      ${( posts.length ) ? '</ul>' : ''}
-      `);
+      $.getJSON( universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
 
-      this.isSpinnerVisible = false;
+        var combinedResults = posts.concat(pages);
 
+        this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${( combinedResults.length ) ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
+          ${combinedResults.map(item=>`<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+        ${( combinedResults.length ) ? '</ul>' : ''}
+        `);
+  
+        this.isSpinnerVisible = false;
+
+      });
     });
   }
 
