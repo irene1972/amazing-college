8afda8bc2558659643a6688988becc2295d9b54d
Irene <ireneog_72@hotmail.es>
2020-01-20 13:22:43 +0100

    Managing time in javascript

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -7,8 +7,10 @@ class Search{
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
 
     this.isOverlayOpen = false;
+    this.typingTimer;
 
     this.events();
   }
@@ -18,10 +20,16 @@ class Search{
     this.openButton.on( "click", this.openOverlay.bind(this) ); //---> el 'bind(this)' es para que cuando estemos dentro de la función 'openOverlay' al hacer mención a this entienda el this referido a la clase en la que estamos y no el this referido al elemento al que hemos hecho click!!
     this.closeButton.on( "click", this.closeOverlay.bind(this) );
     $(document).on("keyup", this.keyPressDispacher.bind(this));
-
+    this.searchField.on("keydown", this.typingLogic.bind(this));
   }
 
   //Methods (function, action...)
+  typingLogic(){
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout( function(){alert('Prueba input search');}, 2000 );
+    
+  }
+
   keyPressDispacher( e ){
     if( e.keyCode == 83 && !this.isOverlayOpen ){
       this.openOverlay();
