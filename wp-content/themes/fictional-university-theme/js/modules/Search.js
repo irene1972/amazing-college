22846a01226a689b88049a2d2e11e77fc87ae738
Irene <ireneog_72@hotmail.es>
2020-01-20 20:57:40 +0100

    Generate html based on json

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -59,8 +59,19 @@ class Search{
   getResults(){
     //this.resultsDiv.html('Imagine real search results here...');
     //this.isSpinnerVisible = false;
-    $.getJSON( 'http://localhost:3000/amazing-college/app/wp-json/wp/v2/posts?search=' + this.searchField.val(), function( posts ){
-      alert(posts[0].title.rendered);
+
+    // $.getJSON( 'http://localhost:3000/amazing-college/app/wp-json/wp/v2/posts?search=' + this.searchField.val(), function( posts ){
+    //   alert(posts[0].title.rendered);
+    // }.bind(this));
+  
+
+    $.getJSON( 'http://localhost:3000/amazing-college/app/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+      this.resultsDiv.html(`
+      <h2 class="search-overlay__section-title">General Information</h2>
+      <ul class="link-list min-list">
+        ${posts.map(item=>`<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+      </ul>
+      `);
     });
   }
 
