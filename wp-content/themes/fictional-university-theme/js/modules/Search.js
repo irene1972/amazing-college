f5a779131714fb210d8616e23f457e820319a83e
Irene <ireneog_72@hotmail.es>
2020-01-20 12:56:38 +0100

    Keyboard events in javascript

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -7,26 +7,42 @@ class Search{
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
+
+    this.isOverlayOpen = false;
+
     this.events();
   }
 
   // Events
   events(){
-    this.openButton.on( 'click', this.openOverlay.bind(this) ); //---> el 'bind(this)' es para que cuando estemos dentro de la función 'openOverlay' al hacer mención a this entienda el this referido a la clase en la que estamos y no el this referido al elemento al que hemos hecho click!!
-    this.closeButton.on( 'click', this.closeOverlay.bind(this) );
+    this.openButton.on( "click", this.openOverlay.bind(this) ); //---> el 'bind(this)' es para que cuando estemos dentro de la función 'openOverlay' al hacer mención a this entienda el this referido a la clase en la que estamos y no el this referido al elemento al que hemos hecho click!!
+    this.closeButton.on( "click", this.closeOverlay.bind(this) );
+    $(document).on("keyup", this.keyPressDispacher.bind(this));
 
   }
 
   //Methods (function, action...)
-  openOverlay(){
-    //console.log(this.searchOverlay)
-    this.searchOverlay.addClass('search-overlay--active');
+  keyPressDispacher( e ){
+    if( e.keyCode == 83 && !this.isOverlayOpen ){
+      this.openOverlay();
+    }
+
+    if( e.keyCode == 27  && this.isOverlayOpen ){
+      this.closeOverlay();
+    }
 
   }
 
-  closeOverlay(){
-    this.searchOverlay.removeClass('search-overlay--active');
+  openOverlay(){
+    this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    this.isOverlayOpen = true;
+  }
 
+  closeOverlay(){
+    this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
   }
 
 }
