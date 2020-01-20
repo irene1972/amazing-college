41a922abc17a3c1c53153a072d6dfe9b2a057d1f
Irene <ireneog_72@hotmail.es>
2020-01-20 19:50:29 +0100

    Waiting-loading spinner icon

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -8,9 +8,13 @@ class Search{
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.searchField = $("#search-term");
+    this.resultsDiv = $("#search-overlay__results");
 
     this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+
     this.typingTimer;
+    this.previousValue;
 
     this.events();
   }
@@ -19,19 +23,46 @@ class Search{
   events(){
     this.openButton.on( "click", this.openOverlay.bind(this) ); //---> el 'bind(this)' es para que cuando estemos dentro de la función 'openOverlay' al hacer mención a this entienda el this referido a la clase en la que estamos y no el this referido al elemento al que hemos hecho click!!
     this.closeButton.on( "click", this.closeOverlay.bind(this) );
-    $(document).on("keyup", this.keyPressDispacher.bind(this));
-    this.searchField.on("keydown", this.typingLogic.bind(this));
+    $(document).on("keydown", this.keyPressDispacher.bind(this));
+    this.searchField.on("keyup", this.typingLogic.bind(this));
   }
 
   //Methods (function, action...)
   typingLogic(){
-    clearTimeout(this.typingTimer);
-    this.typingTimer = setTimeout( function(){alert('Prueba input search');}, 2000 );
-    
+
+    if( this.searchField.val() != this.previousValue ){
+
+      clearTimeout(this.typingTimer);
+      
+      if( this.searchField.val() ){
+
+        if( !this.isSpinnerVisible ){
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        
+        this.typingTimer = setTimeout( this.getResults.bind(this), 2000 );
+
+      }else{
+
+        this.resultsDiv.html('');
+        this.isSpinnerVisible = false;
+
+      }
+        
+    }
+
+    this.previousValue = this.searchField.val();
+      
+  }
+
+  getResults(){
+    this.resultsDiv.html('Imagine real search results here...');
+    this.isSpinnerVisible = false;
   }
 
   keyPressDispacher( e ){
-    if( e.keyCode == 83 && !this.isOverlayOpen ){
+    if( e.keyCode == 83 && !this.isOverlayOpen && $("input, textarea").is(":focus") ){
       this.openOverlay();
     }
 
