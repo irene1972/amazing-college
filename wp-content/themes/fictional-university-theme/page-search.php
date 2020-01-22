b49e60a0972a3d6ca9aa5cd53718d9f8ffbd1278
Irene <ireneog_72@hotmail.es>
2020-01-22 10:31:47 +0100

    Finish traditional wordpress searching

--- wp-content/themes/fictional-university-theme/page-search.php
+++ wp-content/themes/fictional-university-theme/page-search.php
@@ -59,14 +59,9 @@ while( have_posts() ){
     <?php endif; ?>
 
     <div class="generic-content">
-        <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-          <label class="headline headline--medium" for="s">Perform a New Search</label>
-          <div class="search-form-row">
-            <input class="s" type="search" id="s" name="s" placeholder="What are you looking for?">
-            <input class="search-submit" type="submit" value="Search">
-          </div>
-          
-        </form>
+
+        <?php get_search_form(); ?>
+        
     </div>
 
   </div>
