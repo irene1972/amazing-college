f8e0dc0735d278335b2b8d790926346b3afece6a
Irene <ireneog_72@hotmail.es>
2020-01-22 23:11:01 +0100

    Finish Delete note -rest api- & init Update note

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -3,32 +3,38 @@ import $ from 'jquery';
 class MyNotes{
 
   constructor(){
-
     this.editButton = $(".edit-note");
     this.deleteButton = $(".delete-note");
     this.events();
   }
 
   events(){
-    this.editButton.on( "click", this.editNote.bind(this) );
-    this.deleteButton.on( "click", this.deleteNote.bind(this) );
+    this.editButton.on( "click", this.editNote );
+    this.deleteButton.on( "click", this.deleteNote );
   }
 
-  editNote(){
-    alert('Editamos!!');
+  editNote( event ){
+    var elemLiNote = $(event.target).parents("li");
+    //var id = elemLiNote.data('id');
+
+    elemLiNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+    elemLiNote.find(".update-note").addClass("update-note--visible");
   }
 
-  deleteNote(){
-    alert('Eliminamos!!');
+  deleteNote( event ){
+    var elemLiNote = $(event.target).parents("li");
+    var id = elemLiNote.data('id');
+
     $.ajax({
       //http://localhost:3000/amazing-college/app/wp-json/wp/v2/note
       //http://localhost:3000/amazing-college/app/wp-json/wp/v2/note/95
       beforeSend: (xhr) => {
         xhr.setRequestHeader( 'X-WP-Nonce', universityData.nonce );
       },
-      url: universityData.root_url + '/wp-json/wp/v2/note/95',
+      url: universityData.root_url + '/wp-json/wp/v2/note/' + id,
       type: 'DELETE',
       success: ( response ) => {
+        elemLiNote.slideUp();
         console.log('Congratulations');
         console.log( response );
       },
--- wp-content/themes/fictional-university-theme/page-my-notes.php
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -30,11 +30,12 @@ while( have_posts() ){
           $user_notes->the_post();
 
           ?>
-            <li>
-              <input value="<?= esc_attr(get_the_title()) ?>" class="note-title-field">
+            <li data-id="<?= the_ID() ?>">
+              <input value="<?= esc_attr(get_the_title()) ?>" class="note-title-field" readonly />
               <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
               <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-              <textarea class="note-body-field"><?= esc_attr(get_the_content()) ?></textarea>
+              <textarea class="note-body-field" readonly ><?= esc_attr(get_the_content()) ?></textarea>
+              <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
             </li>
           <?php
 
