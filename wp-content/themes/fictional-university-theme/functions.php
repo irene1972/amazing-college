7d43bd0de4aab604fdc51dd865ece5da55e3cab8
Irene <ireneog_72@hotmail.es>
2020-01-23 22:14:30 +0100

    My-Notes premisions and security part 2

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -223,7 +223,13 @@ add_action( 'login_enqueue_scripts', 'our_login_css' );
 
 
 // Force note posts to be private
+// Esta función intercepta una solicitud justo antes de que los datos se guarden en una base de datos
 function makeNotePrivate ( $data ){
+
+  if( $data['post_type'] == 'note' ){
+    $data['post_title'] = sanitize_text_field( $data['post_title'] );
+    $data['post_content'] = sanitize_textarea_field( $data['post_content'] );
+  }
 
   if( $data['post_type'] == 'note' && $data['post_status'] != 'trash' )
      $data['post_status'] = 'private';
