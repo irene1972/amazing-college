c5052b145d131f77023f754bd28f4e74c01d100a
Irene <ireneog_72@hotmail.es>
2020-01-23 22:53:30 +0100

    Security Post limit per-user

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -224,11 +224,16 @@ add_action( 'login_enqueue_scripts', 'our_login_css' );
 
 // Force note posts to be private
 // Esta función intercepta una solicitud justo antes de que los datos se guarden en una base de datos
-function makeNotePrivate ( $data ){
+function makeNotePrivate ( $data, $postarr ){
 
   if( $data['post_type'] == 'note' ){
+
+    if( count_user_posts( get_current_user_id(), 'note' ) > 3 && !$postarr['ID'] )
+      die("You have reached your note limit.");
+
     $data['post_title'] = sanitize_text_field( $data['post_title'] );
     $data['post_content'] = sanitize_textarea_field( $data['post_content'] );
+
   }
 
   if( $data['post_type'] == 'note' && $data['post_status'] != 'trash' )
@@ -238,6 +243,6 @@ function makeNotePrivate ( $data ){
 
 }
 
-add_filter( 'wp_insert_post_data', 'makeNotePrivate' );
+add_filter( 'wp_insert_post_data', 'makeNotePrivate', 10, 2 );
 
 ?>
